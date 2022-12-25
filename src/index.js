42fa4f17aad496c5a0f3189eb9bf8065ca1ede29
mdrizwan7739 <85256728+mdrizwan7739@users.noreply.github.com>
2022-12-25 21:26:48 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
+class API {
+    #secure = false;
+    constructor(url, method = 'GET') {
+        this.url = url;
+        this.method = 'GET';
+    }
+    isSecure() {
+        let a = this.url.indexOf("s");
+        if (a != 4) {
 
+            return this.#secure;
+        }
+        else {
+            this.#secure = true;
+            return this.#secure;
+        }
+    }
+    updateUrl(url) {
+        this.url = url;
+    }
+
+
+}
 
 module.exports = { API }
